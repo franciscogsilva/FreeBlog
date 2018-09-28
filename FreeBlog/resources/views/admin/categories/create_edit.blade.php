418465fd286fa9a458ee7621368c5cc270574dc4
Francisco GSilva <javiergonzalezsilva@gmail.com>
2018-09-28 18:00:25 -0500

    CMS - Categories views done

---
+++ FreeBlog/resources/views/admin/categories/create_edit.blade.php
@@ -0,0 +1,71 @@
+@extends('admin.layouts.admin_layout')
+
+@section('content')
+	<div class="container container-fgs">
+        <div class="row">	
+			<div class="col s12 m12 l12">
+				<div class="card-panel card-panel-table-factories">
+			        <div class="head-edit">
+			            <div class="section">              
+							@if(isset($category))
+								<p class="caption-title center-align">Editar Categoria</p>
+							@else 
+			               		<p class="caption-title center-align">Crear nueva Categoria</p>
+							@endif
+			            </div>
+        				@include('admin.layouts.partials._messages')
+			            <div class="section ">
+			                @if(isset($category))
+			                    {!! Form::open(['route' => ['categories.update', $category->id], 'method' => 'PUT', 'onsubmit' => 'preloader()']) !!}
+			                @else 
+			                    {!! Form::open(['route' => 'categories.store', 'method' => 'POST', 'onsubmit' => 'preloader()']) !!}
+			                @endif
+			                	<div class="row">
+				                    <div class="input-field col s12 m6 l6">
+				                        <i class="material-icons prefix">subject</i>
+				                        {!! Form::text('name', isset($category)?$category->name:null, ['class' => '', 'required', 'id' => 'name']) !!}
+				                        <label for="name">Nombre de la Categoria</label>
+				                    </div>
+				                </div>
+			                    <div class="buttonpanel-edit center-align">
+			                        <a href="{{ route('categories.index') }}" class="btn waves-effect waves-light grey" onclick="return confirm('¿Desea cancelar la creación la Categoria?')">Cancelar</a>              
+			                      @if(isset($category))
+			                        {!! Form::submit('Editar', ['class' => 'btn waves-effect btn-fgs-edit postulation-btn', 'id' => 'postulation-btn']) !!}
+			                      @else 
+			                        {!! Form::submit('Crear', ['class' => 'btn waves-effect btn-fgs-edit postulation-btn', 'id' => 'postulation-btn']) !!}
+			                      @endif
+									<div class="postulate-preloader" id="postulate-preloader" style="display: none;">
+										<div class="preloader-wrapper big active">
+											<div class="spinner-layer spinner-red-only">
+												<div class="circle-clipper left">
+													<div class="circle"></div>
+												</div>
+												<div class="gap-patch">
+													<div class="circle"></div>
+												</div>
+												<div class="circle-clipper right">
+													<div class="circle"></div>
+												</div>
+											</div>
+										</div>	
+									</div>
+			                    </div>
+			                {!! Form::close() !!}              
+			            </div>
+			        </div>
+				</div>
+			</div>
+        </div>
+    </div>
+@endsection()
+
+@section('js')
+	<script type="text/javascript">
+		function preloader(){
+			Materialize.toast('Cargando recursos, este proceso puede tardar un poco', 2000, 'orange darken-1');
+			$('#postulation-btn').hide();
+			$('.postulation-btn').hide();
+			$('#postulate-preloader').show();
+		}
+	</script>						
+@endsection
---
+++ FreeBlog/resources/views/admin/categories/index.blade.php
@@ -0,0 +1,73 @@
+@extends('admin.layouts.admin_layout')
+
+@section('content')
+	<div class="container container-fgs">
+        <div class="row">	
+			<div class="col s12 m12 l12">	
+                <div class="card-panel card-panel-search-fgs">	
+					@include('admin.layouts.partials._messages')				
+					{!! Form::open(['route' => 'categories.index', 'method' => 'GET'], ['class' => 's12 m12 l12']) !!}
+						<div class="row row-search-fgs">
+							<div class="file-field input-field col s12 m12 l9 input-search-fgs">
+								<div class="file-path-wrapper path-wrapper-fgs center-text">
+									<input id="search" type="text" class="validate" name="search">
+									<label class="label-search-fgs" for="icon_prefix">Buscar categoria por nombre</label>
+								</div>
+							</div>
+		                    	{!! Form::submit('Buscar', ['class' => 'btn btn-search-fgs col s10 m11 l2 btn-fgs-edit']) !!}
+							<div id="btn-filters" class="btn col s2 m1 l1 btn-fgs-filter center-align btn-fgs-edit waves-effect waves-light">
+								<span class="center-align"><i class="material-icons icon-filter">keyboard_arrow_down</i></span>
+							</div>
+						</div>
+						<div class="section section-form">
+                        	<div class="row" id="panel-filters">
+								
+                        	</div>
+                        </div>
+	                {!! Form::close() !!}
+				</div>
+				<div class="card-panel card-panel-table-factories">
+					<div class="card-content">
+						{!! Form::open(['route' => 'categories.multi_destroy', 'method' => 'POST'], ['class' => '']) !!}
+						<div class="row row-delete-all">							
+							<div class="btn red darken-3 right" id="multipleDelete" onclick="multiDelete()">Eliminar Varios</div>							
+							<div class="btn red darken-3 right cancel_multipleDelete" id="cancel_multipleDelete" onclick="cancel_multipleDelete()" style="display: none;"><i class="material-icons">clear</i></div>
+							{!! Form::submit('Eliminar', ['class' => 'btn red darken-3 right', 'onclick' => 'return confirm("¿Desea Eliminar las categorias seleccionados?")', 'id' => 'multiDeleteAction', 'style' => 'display: none;']) !!}	
+						</div>
+			     		<table class="highlight striped">
+							<thead>
+								<th>Nombre</th>
+								<th># Articulos</th>
+								<th class="center-align">Opciones</th>
+							</thead>
+							<tbody>
+								@foreach($categories as $category)
+									<tr>									
+										<td>{{ $category->name }}</td>
+										<td class="center-align">
+											{{ count($category->articles) }}
+										</td>
+										<td class="td-fgs center-align">
+											<div class="btn multi_input_delete" style="display: none;">
+												<input name="items_to_delete[]" type="checkbox" class="filled-in filled-in-fgs" id="input_{{$category->id}}" value="{{$category->id}}"/>
+	      										<label for="input_{{$category->id}}"></label>
+											</div>
+		                					{!! Form::close() !!}
+											<a href="{{ route('categories.destroy', $category->id) }}" onclick="return confirm('¿Desea Eliminar la categoria?')" class="btn btn-fgs btn-fgs-delete red darken-3"><i class="material-icons">visibility_off</i></a>
+											<a href="{{ route('categories.edit', $category->id) }}" class="btn btn-raised btn-primary btn-fgs btn-fgs-edit"><i class="material-icons">create</i></a>
+										</td>
+									</tr>
+								@endforeach
+							</tbody>
+						</table>
+			        </div>
+				</div>
+			</div>
+        </div>
+    </div>
+	<?php $paginator = $categories;?>
+    @include('admin.layouts.partials._paginator')
+    @include('admin.layouts.partials._fixed_button_create')
+@endsection()
+
+@include('admin.layouts.partials._js_filters')
