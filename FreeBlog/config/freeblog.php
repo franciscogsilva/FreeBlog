66ab91172b6f9ea0c979622d248789aa9229f0be
Francisco GSilva <javiergonzalezsilva@gmail.com>
2018-09-28 11:57:21 -0500

    Update to freeblog config file

--- FreeBlog/config/freeblog.php
+++ FreeBlog/config/freeblog.php
@@ -7,4 +7,28 @@ return [
     |------------------------------------------
     */
     'items_per_page_paginator' => 30,
+
+    /*
+    |-----------------------------------------
+    |Max image size (pixels) allowed for banners.
+    |------------------------------------------
+    */
+    'max_width_banner'=>1280,
+    'max_height_banner'=>400,
+
+    /*
+    |-----------------------------------------
+    |Max image size (pixels) allowed for images.
+    |------------------------------------------
+    */
+    'max_width_image'=>800,
+    'max_height_image'=>450,
+
+    /*
+    |-----------------------------------------
+    |Max image size (pixels) allowed for profile images.
+    |------------------------------------------
+    */
+    'max_width_profile_image'=>400,
+    'max_height_profile_image'=>400,
 ];
