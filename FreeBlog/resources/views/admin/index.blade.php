83a66e18364a5a6bc5aa48a3e4f23ea510c2923b
Francisco GSilva <javiergonzalezsilva@gmail.com>
2018-09-28 12:12:08 -0500

    Index admin added

---
+++ FreeBlog/resources/views/admin/index.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layouts.admin_layout')
+
+@section('content')
+	<div class="my-3 my-md-5">
+		<div class="container">
+			<div class="page-header">
+				<h1 class="page-title">Hola Admin</h1>
+			</div>
+		</div>
+	</div>
+@endsection()
