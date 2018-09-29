31d6975db6ba628800849dadb58fad28ed3a6116
Francisco GSilva <javiergonzalezsilva@gmail.com>
2018-09-29 18:33:33 -0500

    WEB Social share support added!

---
+++ FreeBlog/resources/views/front/layouts/partials/_metadatos_social.blade.php
@@ -0,0 +1,5 @@
+<meta name="og:url" 			content="{{ $route }}" />
+<meta name="og:type" 			content="article" />
+<meta name="og:title" 			content="{{ $meta->title }}" />
+<meta name="og:description" 	content="{!!$meta->content !!}" />
+<meta name="og:image" 			content="{{ $meta->image->image }}" />
